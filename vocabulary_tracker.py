ad19e170584adc7ba16577d2138841378aa4fec3
Bruno Araujo de Carvalho <bruharauj@gmail.com>
2023-05-01 10:20:02 -0400

    functions were created in order to clean the code and maintain, files were moved to the directory data

--- vocabulary_tracker.py
+++ vocabulary_tracker.py
@@ -1,6 +1,5 @@
 import re
 from itertools import zip_longest
-import collections
 
 #options
 lang = input(
@@ -13,126 +12,12 @@ lang = input(
          "\nPORTUGUESE"
          "\nENGLISH"
          "\nDATABASE\n").lower()
-valid_languages =["german", "swedish", "italian", "french", "spanish", "portuguese", "english", "database"]
 
-if lang not in valid_languages:
-    print("Please write a valid language")
-else:
-    with open(lang + "_vocabulary_database.txt", "r+") as vocab, open(lang +"_unkown_words.txt", "r+") as new_vocab:
+valid_languages =["german", "swedish", "italian", "french", "spanish", "portuguese", "english"]
 
-        def display():
-            option =input("|---------------------------|"
-                         "\n|_Type|_TEXT_OPTION_________|"
-                         "\n|__c__|_-Count all words____|"
-                         "\n|__w__|_-Count unkown words_|"
-                         "\n|__r__|_-Save_known_words___|"
-                         "\n|__i__|_-Information________|"
-                         "\n|---------------------------|"
-                         "\n"
-                         "\n Please type the letter of your choice:\n")
-            return option
-
-
-        def file_input():
-            filename = input("Enter name of input file: ")
-            try:
-                inputfile = open(filename)
-                read_file = inputfile.read()
-                file_list = read_file.split()
-            except FileNotFoundError:             
-                print("Please make sure that the file's name is correct.")
-            else:
-        #cleaning the words before counting them
-                words_book = []
-                for line in file_list:
-                    line = line.strip().lower().replace("ã¤", "ä").replace("ãÿ", "ß").replace("ã¼", "ü").replace("ã¶", "ö").replace("ã©", "é").replace(" ", "").replace("ã¥", "å").replace("ã¤", "ä").replace("ã¶", "ö")
-                    line = re.sub(r"^.*?['`]", '', line)
-                    line = re.sub('[^A-Za-zåäöüéßùàìèòàçèíóñáâãêëíîïôõúû]', '', line)
-                    words_book.append(line)
-                return words_book
-
-
-        def read_vocab(vocab, new_vocab):    
-            vocab_split = []
-            new_vocab_split = []
-            vocab_split = vocab.read().split()
-            new_vocab_split = new_vocab.read().split()
-            return vocab_split, new_vocab_split
-        #-----------:----------------------Languages-----------------------------------------
-
-    #displaying the options 
-        option = display()
-    #variables
-        count = {}
-        word_count = {}
-        words_book = []
-        new_words = []
-        total = 0
-        new = 0 
-        percent = 0 
-        known = 0
-        formatted_percent = 0
-    #openin the book or text
-        words_book = list(file_input())
-        if words_book is not None:
-            vocab_split, new_vocab_split = read_vocab(vocab, new_vocab)
-    #creatig lists for the vocabulary, unkown words and the book
-    #countig all the words from the book 
-        elif option == "c":
-            for line in words_book:
-                if line in word_count:
-                    word_count[line] += 1
-                else:
-                    word_count[line] = 1
-            sorted_word_count = sorted(word_count.items(),key=lambda x: x[1],reverse=True)
-            print(sorted_word_count)
-    #countig all the unkown words in the book
-        elif option == "w":
-            for line in words_book:
-                if line not in vocab_split:
-                    if line in count:
-                        count[line] += 1
-                    else:
-                        count[line] = 1
-            sorted_word_count = sorted(count.items(),key=lambda x: x[1],reverse=False)
-            print(sorted_word_count)                       
-    #displaing all words for recognition that are neither in the vocabulary file nor in the unkown words file
-        elif option == "r":
-            for line in words_book:
-                if line not in vocab_split and line not in new_vocab_split:
-                    new_words.append(line)
-            chunks = zip_longest(*[iter(new_words)] * 20)
-            for chunk in chunks:                   
-                print(chunk)
-                rec = input("type the words that you know from the list:")
-                rec_split = rec.split()
-                new = input('Please type the words that you want to save the unkown words file:\n')
-                new_split = new.split()
-                for word in rec_split:
-                    vocab.write(str(word) + '\n')
-                for word in new_split:
-                    new_vocab.write(str(word) + '\n')
-                cont = input("do you want to continue?(y/n)").lower()
-                if cont == 'n':
-                    break
-                else:
-                    continue
-    #displaing some information about the book comparing to the database
-        elif option == "i":
-            for word in words_book:
-                total += 1
-                if word in vocab_split:
-                    known += 1
-                else:
-                    new += 1
-            percent = (100*known)/total
-            formatted_percent = "{:.2f}".format(percent)
-            print("the number of unknown words in the book:",new,
-                  "\nthe number of words in the book:",total,
-                  "\n capicity of reading:", formatted_percent, "%")
-
-            
-if lang == 'd':
+#opnening all the files to give how many words there are in order to compare vocabularies 
+#           
+if lang == 'database':
     uenglish_count = 0
     ufrench_count = 0
     ugerman_count = 0
@@ -140,42 +25,29 @@ if lang == 'd':
     uportuguese_count = 0
     uspanish_count = 0
     uswedish_count = 0
-    with open("english_unkown_words.txt","r+") as unkown_english, \
-         open("english_vocabulary_database.txt","r+") as database_english, \
-         open("french_unkown_words.txt","r+") as unkown_french, \
-         open("french_vocabulary_database.txt", "r+") as database_french, \
-         open("german_unkown_words.txt" , "r+") as unkown_german, \
-         open("german_vocabulary_database.txt", "r+") as database_german, \
-         open("italian_unkown_words.txt", "r+") as unkown_italian, \
-         open("italian_vocabulary_database.txt","r+") as database_italian, \
-         open("portuguese_unkown_words.txt","r+") as unkown_portuguese, \
-         open("portuguese_vocabulary_database.txt","r+") as database_portuguese, \
-         open("spanish_unkown_words.txt","r+") as unkown_spanish, \
-         open("spanish_vocabulary_database.txt","r+") as database_spanish, \
-         open("swedish_unkown_words.txt","r+") as unkown_swedish, \
-         open("swedish_vocabulary_database.txt","r+") as database_swedish:
+    with open("data/english_unkown_words.txt","r+") as unkown_english, \
+         open("data/english_vocabulary_database.txt","r+") as database_english, \
+         open("data/french_unkown_words.txt","r+") as unkown_french, \
+         open("data/french_vocabulary_database.txt", "r+") as database_french, \
+         open("data/german_unkown_words.txt" , "r+") as unkown_german, \
+         open("data/german_vocabulary_database.txt", "r+") as database_german, \
+         open("data/italian_unkown_words.txt", "r+") as unkown_italian, \
+         open("data/italian_vocabulary_database.txt","r+") as database_italian, \
+         open("data/portuguese_unkown_words.txt","r+") as unkown_portuguese, \
+         open("data/portuguese_vocabulary_database.txt","r+") as database_portuguese, \
+         open("data/spanish_unkown_words.txt","r+") as unkown_spanish, \
+         open("data/spanish_vocabulary_database.txt","r+") as database_spanish, \
+         open("data/swedish_unkown_words.txt","r+") as unkown_swedish, \
+         open("data/swedish_vocabulary_database.txt","r+") as database_swedish:
 
 
-        read_vocab = unkown_english.read()
-        unkown_english_split = read_vocab.split()
-
-        read_vocab = unkown_french.read()
-        unkown_french_split = read_vocab.split()
-
-        read_vocab = unkown_german.read()
-        unkown_german_split = read_vocab.split()
-
-        read_vocab = unkown_italian.read()
-        unkown_italian_split = read_vocab.split()
-
-        read_vocab = unkown_portuguese.read()
-        unkown_portuguese_split = read_vocab.split()
-
-        read_vocab = unkown_spanish.read()
-        unkown_spanish_split = read_vocab.split()
-
-        read_vocab = unkown_swedish.read()
-        unkown_swedish_split = read_vocab.split()
+        unkown_english_split = unkown_english.read().split()
+        unkown_french_split= unkown_french.read().split()
+        unkown_german_split = unkown_german.read().split()
+        unkown_italian_split = unkown_italian.read().split()
+        unkown_portuguese_split = unkown_portuguese.read().split()
+        unkown_spanish_split = unkown_spanish.read().split()
+        unkown_swedish_split = unkown_swedish.read().split()
 
         for word in unkown_english_split:
             uenglish_count += 1
@@ -200,26 +72,13 @@ if lang == 'd':
         dspanish_count = 0
         dswedish_count = 0
 
-        read_vocab = database_english.read()
-        database_english_split = read_vocab.split()
-
-        read_vocab = database_french.read()
-        database_french_split = read_vocab.split()
-
-        read_vocab = database_german.read()
-        database_german_split = read_vocab.split()
-
-        read_vocab = database_italian.read()
-        database_italian_split = read_vocab.split()
-
-        read_vocab = database_portuguese.read()
-        database_portuguese_split = read_vocab.split()
-
-        read_vocab = database_spanish.read()
-        database_spanish_split = read_vocab.split()
-
-        read_vocab = database_swedish.read()
-        database_swedish_split = read_vocab.split()
+        database_english_split = database_english.read().split()
+        database_french_split = database_french.read().split()
+        database_german_split= database_german.read().split()
+        database_italian_split = database_italian.read().split()
+        database_portuguese_split = database_portuguese.read().split()
+        database_spanish_split = database_spanish.read().split()
+        database_swedish_split = database_swedish.read().split()
 
         for word in database_english_split:
             denglish_count += 1
@@ -239,7 +98,7 @@ if lang == 'd':
         
 
 
-
+#printing how many words there are in the database and unkown words files
 
         print("here is the list of languages and the number of words that you know:",
               "\nENGLISH:    ",denglish_count,
@@ -257,3 +116,135 @@ if lang == 'd':
               "\nPORTUGUESE: ",uportuguese_count,
               "\nSPANISH:    ",uspanish_count,
               "\nSWEDISH:    ",uswedish_count,)
+
+elif lang not in valid_languages:
+    print("Please write a valid language")
+
+else:
+    with open("data/" + lang + "_vocabulary_database.txt", "r+") as vocab, open("data/" + lang +"_unkown_words.txt", "r+") as new_vocab:
+
+        def display():
+            option =input("|---------------------------|"
+                         "\n|_Type|_TEXT_OPTION_________|"
+                         "\n|__c__|_-Count all words____|"
+                         "\n|__w__|_-Count unkown words_|"
+                         "\n|__r__|_-Save_known_words___|"
+                         "\n|__i__|_-Information________|"
+                         "\n|---------------------------|"
+                         "\n"
+                         "\n Please type the letter of your choice:\n")
+            return option
+
+#function to open the file that will have to be in the same directory as the code
+
+        def file_input():
+            filename = input("Enter name of input file: ")
+            try:
+                inputfile = open(filename)
+                read_file = inputfile.read()
+                file_list = read_file.split()
+            except FileNotFoundError:             
+                print("Please make sure that the file's name is correct.")
+            else:
+
+#cleaning the words of any language in the list 
+
+                words_book = []
+                for line in file_list:
+                    line = line.strip().lower().replace("ã¤", "ä").replace("ãÿ", "ß").replace("ã¼", "ü").replace("ã¶", "ö").replace("ã©", "é").replace(" ", "").replace("ã¥", "å").replace("ã¤", "ä").replace("ã¶", "ö")
+                    line = re.sub(r"^.*?['`]", '', line)
+                    line = re.sub('[^A-Za-zåäöüéßùàìèòàçèíóñáâãêëíîïôõúû]', '', line)
+                    words_book.append(line)
+                return words_book
+
+#reading the vocabularies 
+
+        def read_vocab(vocab, new_vocab):    
+            vocab_split = []
+            new_vocab_split = []
+            vocab_split = vocab.read().split()
+            new_vocab_split = new_vocab.read().split()
+            return vocab_split, new_vocab_split
+#----------------------------------Languages-----------------------------------------
+
+#displaying the options 
+
+        option = display()
+
+#variables
+
+        count = {}
+        word_count = {}
+        words_book = []
+        new_words = []
+        total = 0
+        new = 0 
+        percent = 0 
+        known = 0
+        formatted_percent = 0
+
+#returning the data from the function file_input and making it a list 
+
+        words_book = list(file_input())        
+        vocab_split, new_vocab_split = read_vocab(vocab, new_vocab)
+
+#countig all the words from the book 
+
+        if option == "c":
+            for line in words_book:
+                if line in word_count:
+                    word_count[line] += 1
+                else:
+                    word_count[line] = 1
+            sorted_word_count = sorted(word_count.items(),key=lambda x: x[1],reverse=True)
+            print(sorted_word_count)
+
+#countig all the unkown words in the book
+
+        elif option == "w":
+            for line in words_book:
+                if line not in vocab_split:
+                    if line in count:
+                        count[line] += 1
+                    else:
+                        count[line] = 1
+            sorted_word_count = sorted(count.items(),key=lambda x: x[1],reverse=False)
+            print(sorted_word_count)      
+
+#displaing all words for recognition that are neither in the vocabulary file nor in the unkown words file
+
+        elif option == "r":
+            for line in words_book:
+                if line not in vocab_split and line not in new_vocab_split:
+                    new_words.append(line)
+            chunks = zip_longest(*[iter(new_words)] * 20)
+            for chunk in chunks:                   
+                print(chunk)
+                rec = input("type the words that you know from the list:")
+                rec_split = rec.split()
+                new = input('Please type the words that you want to save the unkown words file:\n')
+                new_split = new.split()
+                for word in rec_split:
+                    vocab.write(str(word) + '\n')
+                for word in new_split:
+                    new_vocab.write(str(word) + '\n')
+                cont = input("do you want to continue?(y/n)").lower()
+                if cont == 'n':
+                    break
+                else:
+                    continue
+
+#displaing some information about the book comparing to the database
+
+        elif option == "i":
+            for word in words_book:
+                total += 1
+                if word in vocab_split:
+                    known += 1
+                else:
+                    new += 1
+            percent = (100*known)/total
+            formatted_percent = "{:.2f}".format(percent)
+            print("the number of unknown words in the book:",new,
+                  "\nthe number of words in the book:",total,
+                  "\n capicity of reading:", formatted_percent, "%")
